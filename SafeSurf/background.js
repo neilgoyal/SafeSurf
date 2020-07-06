2160831819318c65a127145042e3d636d54576e3
Neil Goyal <54841976+Neildagr8@users.noreply.github.com>
2020-07-06 19:27:07 +0530

    Update background.js

--- SafeSurf/background.js
+++ SafeSurf/background.js
@@ -1,6 +1,6 @@
 chrome.extension.onRequest.addListener(function(prediction){
     if (prediction == 1){
-        alert("Warning: Phishing detected!!");
+        alert("Phishing detected");
     }
     else if (prediction == -1){
         alert("No phishing detected");
