d11d1de50423c4f5a89bb6c55d7f0b5780897c42
Neil Goyal <54841976+Neildagr8@users.noreply.github.com>
2020-07-10 22:59:20 +0530

    Update content.js

--- SafeSurf - 1.1/content.js
+++ SafeSurf - 1.1/content.js
@@ -1,6 +1,7 @@
 var testdata;
 var prediction;
 
+// Checks if '@' is in URL
 function isAlphaNumericURL(){
     var search ="@";
     var url = window.location.href;
@@ -14,6 +15,7 @@ function isAlphaNumericURL(){
     }
 }
 
+// Checks if links in anchor tags <a></a> are loaded from different domains
 function isAnchorFromDifferentDomain(){
     var totalCount = document.querySelectorAll("a").length
     var identicalCount = getIdenticalDomainCount("a");
@@ -31,6 +33,7 @@ function isAnchorFromDifferentDomain(){
     }
 }
 
+//Checks if the favicon is loaded from another domain
 function isFaviconDomainUnidentical(){
     var reg = /[a-zA-Z]\//;
     var url = window.location.href;
@@ -51,6 +54,7 @@ function isFaviconDomainUnidentical(){
     }
 }
 
+//Detect invalid/blank form submissions
 function isFormActionInvalid(){
     var totalCount = document.querySelectorAll("form").length
     var identicalCount = getIdenticalDomainCount("form");
@@ -72,6 +76,7 @@ function isFormActionInvalid(){
     }
 }
 
+//Check for '-' in URL
 function isHypenURL(){
     var reg = /[a-zA-Z]\//;
     var srch ="-";
@@ -85,6 +90,7 @@ function isHypenURL(){
         return 1;
     }
 }
+
 
 function getIdenticalDomainCount(tag){
     var i;
@@ -128,6 +134,7 @@ function getIdenticalDomainCount(tag){
     return identicalCount;
 }
 
+//Check if site is uding IFrame
 function isIframePresent(){
     if(document.querySelectorAll('iframe').length<=0){
         console.log("NP");
@@ -139,6 +146,7 @@ function isIframePresent(){
     }
 }
 
+//Identifies if URL has multiple HTTPS
 function isIllegalHttpsURL(){
     var srch1 ="//";
     var srch2 = "https";
@@ -153,6 +161,7 @@ function isIllegalHttpsURL(){
     }
 }
 
+//Checks if images are loaded from different domains
 function isImgFromDifferentDomain(){
     var totalCount = document.querySelectorAll("img").length
     var identicalCount = getIdenticalDomainCount("img");
@@ -170,6 +179,7 @@ function isImgFromDifferentDomain(){
     }
 }
 
+//Does URL Have IP address in it
 function isIPInURL(){
     var reg =/\d{1,3}[\.]{1}\d{1,3}[\.]{1}\d{1,3}[\.]{1}\d{1,3}/;
     var url = window.location.href
@@ -183,6 +193,7 @@ function isIPInURL(){
     }
 }
 
+//Checks if length of URL is more than 75 characters
 function isLongURL(){
     var url = window.location.href;
     if(url.length<54){
@@ -210,6 +221,8 @@ function isMailToAvailable(){
     }
 }
 
+
+// Checks if anchor tag has mailto
 function isMultiDomainURL(){
     var reg = /[a-zA-Z]\//;
     var srch ="-";
@@ -223,6 +236,8 @@ function isMultiDomainURL(){
         return 1;
     }
 }
+
+//Checks if URL is redirecting using //
 function isRedirectingURL(){
     var reg1 = /^http:/
     var reg2 = /^https:/
@@ -242,6 +257,7 @@ function isRedirectingURL(){
     }
 }
 
+//Checks if scripts are loaded from different domain
 function isScLnkFromDifferentDomain(){
     var totalCount = document.querySelectorAll("script").length + document.querySelectorAll("link").length
     var identicalCount = getIdenticalDomainCount("script") + getIdenticalDomainCount("link");
@@ -259,6 +275,7 @@ function isScLnkFromDifferentDomain(){
     }
 }
 
+//Checks if phisher has tampered with status bar
 function isStatusBarTampered(){
     if((document.querySelectorAll("a[onmouseover*='window.status']").length<=0) || (document.querySelectorAll("a[onclick*='location.href']").length<=0)){
         console.log("NP");
@@ -270,6 +287,7 @@ function isStatusBarTampered(){
     }
 }
 
+//Does URL have less than 20 characters
 function isTinyURL(){
     var url = window.location.href;
     if(url.length>20){
@@ -282,19 +300,26 @@ function isTinyURL(){
     }
 }
 
+
 function predict(data,weight){
     var f = 0;
+    // weights of trained SVM
     weight = [3.33346292e-01,-1.11200396e-01,-7.77821806e-01,1.11058590e-01,3.89430647e-01,1.99992062e+00,4.44366975e-01,-2.77951957e-01,-6.00531647e-05,3.33200243e-01,2.66644002e+00,6.66735991e-01,5.55496098e-01,5.57022408e-02,2.22225591e-01,-1.66678858e-01];
+    // Computing forward pass to determine of phishing or not
     for(var j=0;j<data.length;j++) {
       f += data[j] * weight[j];
     }
     return f > 0 ? 1 : -1;
 }
 
+//array of the data collected from website
 testdata = [isIPInURL(),isLongURL(),isTinyURL(),isAlphaNumericURL(),isRedirectingURL(),isHypenURL(),isMultiDomainURL(),isFaviconDomainUnidentical(),isIllegalHttpsURL(),isImgFromDifferentDomain(),isAnchorFromDifferentDomain(),isScLnkFromDifferentDomain(),isFormActionInvalid(),isMailToAvailable(),isStatusBarTampered(),isIframePresent()];
 
+//predicts phishing or not
 prediction = predict(testdata);
 
+
+// Sends prediction to background.js
 chrome.extension.sendRequest(prediction);
 
 
