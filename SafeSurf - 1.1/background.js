83bd7b0b327f22d1c39c4f7b48a9db159402aa48
Neil Goyal <54841976+Neildagr8@users.noreply.github.com>
2020-07-10 22:46:10 +0530

    Update background.js

--- SafeSurf - 1.1/background.js
+++ SafeSurf - 1.1/background.js
@@ -1,3 +1,5 @@
+// gets prediction from content.js
+
 chrome.extension.onRequest.addListener(function(prediction){
     if (prediction == 1){
         chrome.browserAction.setPopup({popup: 'popup_mal.html'})
